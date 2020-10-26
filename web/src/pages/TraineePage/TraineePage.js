50cf8e4dab0d5118d0b575112f46e08c6d4b70f0
ncucciniello <ncucciniello@gmail.com>
2020-10-26 16:43:41 -0400

    add details to workout static element

--- web/src/pages/TraineePage/TraineePage.js
+++ web/src/pages/TraineePage/TraineePage.js
@@ -4,7 +4,16 @@ const TraineePage = () => {
   return (
     <UserLayout>
       <div className="workoutGraphContatiner ">
+        <div className="workoutGraphHeader">
+          <button>Previous Day</button>
+          <h3>Todays Date</h3>
+          <button>Next Day</button>
+        </div>
         <div className="workoutGraph">Workout Graph Goes here</div>
+        <div className="workoutGraphSidebar">
+          <button>Log Workout</button>
+          <button>Edit Log</button>
+        </div>
       </div>
       <div className="exerciseProgressContatiner ">
         <div className="exerciseProgress">
--- web/src/pages/TrainerPage/TrainerPage.js
+++ web/src/pages/TrainerPage/TrainerPage.js
@@ -4,7 +4,10 @@ import UserLayout from 'src/layouts/UserLayout'
 const TrainerPage = () => {
   return (
     <UserLayout>
-      <h3>Client List</h3>
+      <div className="clientListHeader">
+        <h3>Client List</h3>
+        <button>Add Client +</button>
+      </div>
       <div className="clientList">
         <div className="clientListItem">Client 1</div>
         <div className="clientListItem">Client 2</div>
@@ -15,7 +18,16 @@ const TrainerPage = () => {
         <div className="clientListItem">Client 7</div>
       </div>
       <div className="workoutGraphContatiner ">
+        <div className="workoutGraphHeader">
+          <button>Previous Day</button>
+          <h3>Todays Date</h3>
+          <button>Next Day</button>
+        </div>
         <div className="workoutGraph">Workout Graph Goes here</div>
+        <div className="workoutGraphSidebar">
+          <button>Add Workout</button>
+          <button>Edit Workout</button>
+        </div>
       </div>
       <div className="exerciseProgressContatiner ">
         <div className="exerciseProgress">
